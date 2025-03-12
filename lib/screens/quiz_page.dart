334845717df6f6a7ca8e52e30e814c2641f9cbfd
demystik <demystiktech@gmail.com>
2025-03-12 12:21:08 +0100

    building quize page

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -10,8 +10,65 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold(
-      body: Center(child: Text("Quiz Page"),),
-    ), );
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('HTML Questions'),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 50),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child:  Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                      color: Colors.grey,
+                        offset: Offset(1, 0),
+                        blurRadius: 1.0,
+                        spreadRadius: 3.0,
+                    ),
+                    ]
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Question 0/30'),
+                          Text('Quit'),
+                        ],
+                      ),
+                      const Text('Who is making the web standard?'),
+                      Column(
+                        children: [
+                          elevationButton('Option 1'),
+                          elevationButton('Option 2'),
+                          elevationButton('Option 3'),
+                          elevationButton('Option 4'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  elevationButton(String option) {
+    return ElevatedButton(
+      onPressed: (){},
+      child: Text(option),
+    );
   }
 }
